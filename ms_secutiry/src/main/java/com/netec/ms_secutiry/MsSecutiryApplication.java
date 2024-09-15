06a3c5dc261f72fce0c85889adf3bf2219574bf7
Jonathan_Fonseca <jonathan.fonseca@sii-group.co>
2024-09-14 21:48:22 -0500

    se agrega el ms_security

---
+++ ms_secutiry/src/main/java/com/netec/ms_secutiry/MsSecutiryApplication.java
@@ -0,0 +1,13 @@
+package com.netec.ms_secutiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSecutiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSecutiryApplication.class, args);
+	}
+
+}
